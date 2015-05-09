a7caab85d63261d4553496747f460b1ca832543e
rosiv <rosiu@outlook.com>
2015-05-09 09:23:05 -0700

    Add Log4Net

---
+++ src/ODR-Studio.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using log4net;
+
+namespace ODRStudio
+{
+    class MainClass
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Main(string[] args)
+        {
+            log.Debug("Starting application...");
+            log.Info("Starting application...");
+        }
+    }
+}
